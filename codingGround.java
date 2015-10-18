0a60c4f6c3ae460b3d6b0f063550adc4ecf6aa68
guerht <parkseunghoon@gpa.ac.kr>
2015-10-18 01:38:04 -0400

    ADDED FROM CODING GROUND

---
+++ codingGround.java
@@ -0,0 +1,5 @@
+public class codingGround {
+    public static void main(String args[]) {
+        System.out.println("This is to confirm that I was able to upload this file to github from coding ground.");
+    }
+}
